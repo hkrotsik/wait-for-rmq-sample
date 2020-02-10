4838b359ba5f7ac18877a6fbba4eb1cc23d8b41d
Hleb Krotsik <hleb.krotsik@gmail.com>
2020-02-11 00:36:34 +0300

    Initial commit

---
+++ WaitForRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+
+namespace WaitForRabbitMQ.Publisher
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .Build();
+
+            var host = config["HostName"];
+
+            Console.WriteLine($"Using host {host} to publish message");
+
+            // ENV VARIABLE DOESN'T WORK PROPERLY
+            var factory = new ConnectionFactory() { HostName = "rabbitmq" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "hello",
+                                 basicProperties: null,
+                                 body: body);
+
+            Console.WriteLine(" [x] Sent {0}", message);
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WaitForRabbitMQ.Receiver/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace WaitForRabbitMQ.Receiver
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .Build();
+
+            var host = config["HostName"];
+
+            Console.WriteLine($"Using host {host} to receive message");
+
+            // ENV VARIABLE DOESN'T WORK PROPERLY
+            var factory = new ConnectionFactory() { HostName = "rabbitmq" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+
+            channel.BasicConsume(queue: "hello",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
